a3aa3ad1ac7fa84445a3f39e1df9fadcbe4dba94
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-01-31 19:48:01 -0800

    Implement file system retrieval from url parsing

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
 
 http
   .createServer((req, res) => {
     const q = url.parse(req.url, true);
 
-    switch(q.pathname) {
-      case '/':
-        return res.end('/index.html');
-      case '/about':
-        return res.end('/about.html');
-      case '/contact-me':
-        return res.end('/contact-me.html');
-      default:
-        return res.end('/404.html');
+    let filename = "";
+
+    if (q.pathname === "/") {
+      filename = "./index.html";
+    } else {
+      filename = "." + q.pathname + ".html";
     }
+
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        fs.readFile("./404.html", (e, d) => {
+          if (e) {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.write("<h1>404 Not Found</h1><p>Custom 404 page missing</p>");
+          } else {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.write(d);
+          }
+          return res.end();
+        });
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
   })
   .listen(8080);
