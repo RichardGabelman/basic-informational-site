bebf9d8e03e18d4830db7d142bc10c748584fc8c
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-02-02 20:49:31 -0800

    Refactor to use Express.js

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
 
-http
-  .createServer((req, res) => {
-    const q = url.parse(req.url, true);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-    let filename = "";
+app.get("/:name", (req, res) => {
+  const filePath = path.join(__dirname, req.params.name + ".html");
 
-    if (q.pathname === "/") {
-      filename = "./index.html";
-    } else {
-      filename = "." + q.pathname + ".html";
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).sendFile(path.join(__dirname, "404.html"));
     }
+  });
+});
 
-    fs.readFile(filename, (err, data) => {
-      if (err) {
-        fs.readFile("./404.html", (e, d) => {
-          if (e) {
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.write("<h1>404 Not Found</h1><p>Custom 404 page missing</p>");
-          } else {
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.write(d);
-          }
-          return res.end();
-        });
-        return;
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(8080);
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
